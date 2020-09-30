909ab8b0902b90a1d0e275d819d4b7bce84e50d2
tobiaszwinger <tobiaszwinger.tz@gmail.com>
2020-09-30 15:39:38 +0200

    master: add person ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {AddtourComponent} from "./addtour/addtour.component";
-import {PersonsComponent} from "./persons/persons.component";
+import {HomeComponent} from './home/home.component';
+import {AddtourComponent} from './addtour/addtour.component';
+import {PersonsComponent} from './persons/persons.component';
 
 
 const routes: Routes = [
--- src/app/persons/person-ui/person-ui.component.ts
+++ src/app/persons/person-ui/person-ui.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PersonService} from '../../services/person.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-person-ui',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonUIComponent implements OnInit {
 
-  constructor() { }
+  constructor(private personService: PersonService,
+              private _snackBar: MatSnackBar,
+              private router: Router) { }
+
+  addPerson;
+  newPerson = {
+    firstname: null,
+    lastname: null,
+  };
 
   ngOnInit(): void {
+    this.addPerson = false;
   }
 
+  onFirstnameChange(value: any): void {this.newPerson.firstname = value; }
+  onLastnameChange(value: any): void {this.newPerson.lastname = value; }
+
+  openAddPerson(): void {
+    this.addPerson = true;
+  }
+
+  openSnackBar(message: string, action: string): void {
+    this._snackBar.open(message, action, {
+      duration: 3000,
+    });
+  }
+
+  handleSubmit(): void {
+    if (this.newPerson.firstname && this.newPerson.lastname) {
+      this.personService.addPerson(this.newPerson);
+      this.addPerson = false;
+      this.router.navigate(['./persons'])
+        .then(() => {
+          window.location.reload();
+        });
+    } else {
+      this.openSnackBar('Please fill in data', 'close');
+    }
+  }
 }
